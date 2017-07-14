3d042009637224db0af30aa7db1fc9709563c635
nasko <nasko@htmlbox.net>
2017-07-14 15:35:07 +0300

    fix namespace in loader file

--- carbon-fields-plugin.php
+++ carbon-fields-plugin.php
@@ -13,9 +13,9 @@
  * Domain Path: /languages
  */
 
-define( 'Carbon_Fields\PLUGIN_FILE', __FILE__ );
+define( 'Carbon_Fields_Plugin\PLUGIN_FILE', __FILE__ );
 
-define( 'Carbon_Fields\RELATIVE_PLUGIN_FILE', basename( dirname( \Carbon_Fields\PLUGIN_FILE ) ) . '/' . basename( \Carbon_Fields\PLUGIN_FILE ) );
+define( 'Carbon_Fields_Plugin\RELATIVE_PLUGIN_FILE', basename( dirname( \Carbon_Fields_Plugin\PLUGIN_FILE ) ) . '/' . basename( \Carbon_Fields_Plugin\PLUGIN_FILE ) );
 
 add_action( 'after_setup_theme', 'carbon_fields_boot_plugin' );
 function carbon_fields_boot_plugin() {
@@ -25,6 +25,6 @@ function carbon_fields_boot_plugin() {
 	\Carbon_Fields\Carbon_Fields::boot();
 
 	if ( is_admin() ) {
-		\Carbon_Fields\Libraries\Plugin_Update_Warning\Plugin_Update_Warning::boot();
+		\Carbon_Fields_Plugin\Libraries\Plugin_Update_Warning\Plugin_Update_Warning::boot();
 	}
 }
