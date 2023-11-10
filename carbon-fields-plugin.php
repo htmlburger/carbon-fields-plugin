233c2a36dd429610c86383075f6f3e19dc3b04a4
Joro Stoyanov <stoyanov.gs@gmail.com>
2023-11-10 18:01:05 +0200

    Bump v3.6.1

--- carbon-fields-plugin.php
+++ carbon-fields-plugin.php
@@ -2,13 +2,13 @@
 /**
  * Plugin Name: Carbon Fields
  * Description: WordPress developer-friendly custom fields for post types, taxonomy terms, users, comments, widgets, options, navigation menus and more.
- * Version: 3.6.0
+ * Version: 3.6.1
  * Author: htmlburger
  * Author URI: https://htmlburger.com/
  * Plugin URI: http://carbonfields.net/
  * License: GPL2
  * Requires at least: 5.0
- * Tested up to: 6.2
+ * Tested up to: 6.4
  * Text Domain: carbon-fields
  * Domain Path: /languages
  */
